ac682afb9d4602d9c907a8b8a296014467702597
rcanat <rcanat7@gmail.com>
2026-04-28 12:29:09 -0400

    Wrote some tests for MovieEditor

--- tests/MovieEditor.spec.tsx
+++ tests/MovieEditor.spec.tsx
@@ -1,6 +1,7 @@
 import type { Movie } from "../src/interfaces/movie";
 import { MovieEditor } from "../src/components/MovieEditor";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 describe("MovieEditor Component", () => {
     const mockMovie: Movie = {
@@ -37,5 +38,71 @@ describe("MovieEditor Component", () => {
         const title = screen.getByDisplayValue("The Test Movie");
 
         expect(title).toBeInTheDocument();
+    });
+
+    test("changes movie description", () => {
+        const description = screen.getByDisplayValue("A movie for testing");
+        userEvent.type(description, " things");
+        expect(description).toHaveValue("A movie for testing things");
+    });
+
+    test("changes movie released year", () => {
+        const released = screen.getByDisplayValue("2020");
+        userEvent.type(released, "{backspace}{backspace}19");
+        expect(released).toHaveValue(2019);
+    });
+
+    test("changes movie title", () => {
+        const title = screen.getByDisplayValue("The Test Movie");
+        userEvent.type(title, " 2");
+        expect(title).toHaveValue("The Test Movie 2");
+    });
+
+    test("changes movie rating", () => {
+        const rating = screen.getByDisplayValue("⭐⭐⭐⭐✰");
+        userEvent.selectOptions(rating, "⭐⭐✰✰✰");
+        expect(rating).toHaveValue("4");
+    });
+
+    test("cancel button", () => {
+        const description = screen.getByDisplayValue("A movie for testing");
+        const released = screen.getByDisplayValue("2020");
+        const cancel = screen.getByRole("button", {name: "Cancel"})
+        userEvent.type(description, " things");
+        userEvent.type(released, "{backspace}{backspace}19");
+        userEvent.click(cancel);
+
+        expect(mockChangeEditing).toHaveBeenCalledTimes(1);
+        expect(mockEditMovie).toHaveBeenCalledTimes(0);
+        expect(mockDeleteMovie).toHaveBeenCalledTimes(0);
+    });
+
+    test("save button", () => {
+        const description = screen.getByDisplayValue("A movie for testing");
+        userEvent.type(description, " things");
+
+        const released = screen.getByDisplayValue("2020");
+        userEvent.type(released, "{backspace}{backspace}19");
+
+        const title = screen.getByDisplayValue("The Test Movie");
+        userEvent.type(title, " 2");
+        
+        const rating = screen.getByDisplayValue("⭐⭐⭐⭐✰");
+        userEvent.selectOptions(rating, "⭐⭐✰✰✰");
+
+        const save = screen.getByRole("button", {name: "Save"})
+        userEvent.click(save);
+
+
+        expect(mockChangeEditing).toHaveBeenCalledTimes(1);
+        expect(mockEditMovie).toHaveBeenCalledWith(mockMovie.id, {
+            ...mockMovie,
+            title: "The Test Movie 2",
+            released: 2019,
+            rating: 4,
+            description: "A movie for testing things",
+            soundtrack: mockMovie.soundtrack,
+        })
+        expect(mockDeleteMovie).toHaveBeenCalledTimes(0);
     });
 });
