18e3d32f333d751dfa34b7bc3d518e18fddb9976
rcanat <rcanat7@gmail.com>
2026-04-28 15:46:32 -0400

    Disallowed MIME type error fix? (https://github.com/vitejs/vite/discussions/13910)

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: "https://rcanat.github.io/movie-records/",
 });
